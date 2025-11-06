3bfe7c0ca9b033dd12625c82b65377c44ed8c1a9
RIYANDRA04 <riyanmoo95@gmail.com>
2025-11-06 15:41:40 +0800

    fix: dynamic port for Render

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", apiRoutes);
 
-const PORT = 3000;
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`✅ Server running on port ${PORT}`);
 });
